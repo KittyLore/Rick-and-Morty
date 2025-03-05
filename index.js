6d33e857c7b51766a8d965cbea9903b18afc1abd
KittyLore <lorenquiroz407@gmail.com>
2025-03-04 21:04:04 -0500

    2
    CSS

---
+++ index.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    const API_URL = "https://rickandmortyapi.com/api/character";
+    const container = document.getElementById("characters-container");
