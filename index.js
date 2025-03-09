839f4c388a78be119780ad50def5edd96a70aa17
KittyLore <141794841+KittyLore@users.noreply.github.com>
2025-03-09 12:30:14 -0500

    3
    final

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", async () => {
-    const API_URL = "https://rickandmortyapi.com/api/character";
-    const container = document.getElementById("characters-container");
+function tomar(done) {
+    const results = fetch("https://rickandmortyapi.com/api/character");
+    results.then(response => response.json())
+        .then(data => {
+            done(data);
+        });
+}
+
+tomar(data => {
+    data.results.forEach(personaje => {
+        const article = document.createRange().createContextualFragment(/*html*/`
+            <article class="personaje">  <!-- AGREGADO class="personaje" -->
+                <div class="imagen">
+                    <img src="${personaje.image}" alt="Personaje">
+                </div>
+                <h2>${personaje.name}</h2>
+                <h3>Localización: ${personaje.location.name}</h3>
+                <h4>Origen: ${personaje.origin.name}</h4>
+            </article>
+        `);
+        const main = document.querySelector("main");
+        main.append(article);
+    });
+});
